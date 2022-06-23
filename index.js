61c42467004a8399230298af4b29063a10b16dda
Daniel Macedo <danielmsv2011@hotmail.com>
2022-06-22 22:53:30 -0300

    mongoose connect

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
 
 // Read Form JSON / middlewares
-app.use({
-    extended: true,
-})
 
-app.use(express.json())
+app.use(express.json());
+
 
 // Initial Route / endpoint
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+  // show req
 
-    // show req
+  res.json({ message: "Express método GET working" });
+});
 
-    res.json({message: "Express método GET working"})
-
-})
+// entregar uma porta
+mongoose.connect('mongodb+srv://danielmacedo:<password>@apicluster.b9bv4ja.mongodb.net/?retryWrites=true&w=majority')
 
 // Port
-app.listen(3000)
+app.listen(3000);
